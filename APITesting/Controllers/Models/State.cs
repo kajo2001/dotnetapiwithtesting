537889a7ac624d4a50ff7fb82263f7befb3c922f
kajo2001 <kajalbhalerao75@gmail.com>
2026-05-29 12:31:42 +0530

    added

---
+++ APITesting/Controllers/Models/State.cs
@@ -0,0 +1,9 @@
+﻿namespace APITesting.Controllers.Models
+{
+    public class State
+    {
+        public int StateId { get; set; }
+        public string Statename { get; set; }
+
+    }
+}
---
+++ APITesting/Controllers/StateApiController.cs
@@ -0,0 +1,45 @@
+﻿using APITesting.Controllers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITesting.Controllers
+{//  [Route("api/[controller]")]
+    [ApiController]
+    public class StateApiController : ControllerBase
+    {
+
+        [HttpGet()]
+        [Route("api/allstates")]
+        public List<State> GetAllStates()
+        {
+            List<State> lst = GetStates();
+            return lst;
+        }
+        [HttpGet("api/state/{id}")]
+        public State GetState(int id)
+        {
+            State st = GetStates().FirstOrDefault(e => e.StateId.Equals(id));
+            return st;
+        }
+
+
+        [NonAction]
+        public List<State> GetStates()
+        {
+            List<State> states = new List<State>()
+            {
+                new State(){StateId=1,Statename="Maharashtra"},
+                new State(){StateId=2,Statename="Gujarat"},
+                new State(){StateId=3,Statename="Rajasthan"},
+                new State(){StateId=4,Statename="Punjab"},
+                new State(){StateId=5,Statename="Goa"},
+                new State(){StateId=6,Statename="Haryana"},
+                new State(){StateId=7,Statename="Karnataka"},
+                new State(){StateId=8,Statename="MP"},
+                new State(){StateId=9,Statename="UP"},
+                new State(){StateId=10,Statename="Kerala"}
+            };
+            return states;
+        }
+    }
+}
